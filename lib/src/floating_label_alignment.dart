b6f02cf4544fb586e50c2761bd6c7b49e58cc7cd
Muhammad Mohiuddin <muhammad.mohiuddin@live.com>
2024-12-12 23:53:26 +0100

    fix x getter

--- lib/src/floating_label_alignment.dart
+++ lib/src/floating_label_alignment.dart
@@ -2,14 +2,21 @@ import 'package:flutter/material.dart';
 
 extension FloatingLabelAlignmentX on FloatingLabelAlignment {
   double get x {
-    final input = toString();
+    switch (this) {
+      case FloatingLabelAlignment.start:
+        return -1.0;
+      case FloatingLabelAlignment.center:
+        return 0.0;
+      default:
+        final input = toString();
 
-    assert(input.startsWith('FloatingLabelAlignment(x: '));
-    assert(input.endsWith(')'));
+        assert(input.startsWith('FloatingLabelAlignment(x: '));
+        assert(input.endsWith(')'));
 
-    const startIndex = 26;
-    const endIndex = 29;
+        const startIndex = 26;
+        const endIndex = 29;
 
-    return double.parse(input.substring(startIndex, endIndex));
+        return double.parse(input.substring(startIndex, endIndex));
+    }
   }
 }
