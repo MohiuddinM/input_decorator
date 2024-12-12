3fb23e5766c2c72dc7319245d79a9accb12d8eb4
Muhammad Mohiuddin <muhammad.mohiuddin@live.com>
2024-12-12 23:36:42 +0100

    export flutter material

--- lib/input_decorator.dart
+++ lib/input_decorator.dart
@@ -2,3 +2,15 @@ export 'src/input_decorator.dart';
 export 'src/text_field.dart';
 export 'src/text_form_field.dart';
 export 'src/dropdown.dart';
+export 'package:flutter/material.dart'
+    hide
+        InputDecoration,
+        InputDecorator,
+        DropdownButton,
+        DropdownButtonFormField,
+        DropdownButtonHideUnderline,
+        DropdownMenuItem,
+        DropdownButtonBuilder,
+        InputCounterWidgetBuilder,
+        TextField,
+        TextFormField;
